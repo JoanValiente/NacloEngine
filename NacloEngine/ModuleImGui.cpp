8277b8666d4dc51271c7a20d1ba3f78fd4735c45
DavidTheMaaster <davidthegaamer@gmail.com>
2018-09-29 18:06:02 +0200

    Added Glew library

--- NacloEngine/ModuleImGui.cpp
+++ NacloEngine/ModuleImGui.cpp
@@ -13,7 +13,6 @@
 #include "PanelRandomNumberGenerator.h"
 
 
-
 #include <time.h>
 
 
--- NacloEngine/ModuleRenderer3D.cpp
+++ NacloEngine/ModuleRenderer3D.cpp
@@ -6,6 +6,7 @@
 #include <gl/GL.h>
 #include <gl/GLU.h>
 
+#pragma comment (lib, "GL/lib//glew32.lib")
 #pragma comment (lib, "glu32.lib")    /* link OpenGL Utility lib     */
 #pragma comment (lib, "opengl32.lib") /* link Microsoft OpenGL lib   */
 
