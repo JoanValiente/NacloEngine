e8b546f93756fab7f9da2a2f0ada126ff4b9f1d4
DavidTheMaaster <davidthegaamer@gmail.com>
2018-10-03 02:01:12 +0200

    Solved plane and sphere bug
    When you have an sphere and a plane at the same time, the engine
    crashed.

--- NacloEngine/Primitive.cpp
+++ NacloEngine/Primitive.cpp
@@ -473,13 +473,9 @@ void sphere::InnerRender() const
 //	glTranslatef(position.x, position.y, position.z);
 
 	glEnableClientState(GL_VERTEX_ARRAY);
-	glEnableClientState(GL_NORMAL_ARRAY);
-	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 
-
-	glNormalPointer(GL_FLOAT, 0, &normals[0]);
 	glVertexPointer(3, GL_FLOAT, 0, &vertices[0]);
-	glTexCoordPointer(2, GL_FLOAT, 0, &texcoords[0]);
+
 	glDrawElements(GL_QUADS, 220, GL_UNSIGNED_SHORT, &indices[0]);
 
 	glPopMatrix();
