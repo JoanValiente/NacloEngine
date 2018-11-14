046cd0e65b0e756128527eb049019f2d910ad1b0
DavidTheMaaster <davidthegaamer@gmail.com>
2018-11-14 09:31:51 +0100

    Added Scene Serialzation Module

---
+++ NacloEngine/SceneSerialization.h
@@ -0,0 +1,16 @@
+#ifndef __SCENESERIALIZATION_H__
+#define __SCENESERIALIZATION_H__
+
+
+class SceneSerialization
+{
+public:
+	SceneSerialization();
+	~SceneSerialization();
+
+private:
+
+};
+
+#endif // !__SCENESERIALIZATION_H__
+
